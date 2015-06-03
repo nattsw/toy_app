c4eb2fa3ae446a0aaa05cb1b353f2bbd30ed767e
Natalie Tay <natalie.tay@gmail.com>
2015-06-03 17:27:16 +0800

    Added new user scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-    render text: "ohai derp"
-  end
-
-  def goodbye
-    render text: "byee derp"
-  end
-
 end
